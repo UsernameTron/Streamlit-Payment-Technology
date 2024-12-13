cdfae03dad84168a63ce921b83b036be84074193
UsernameTron <cpeteconnor@gmail.com>
2024-12-13 13:16:45 -0600

    Update Streamlit app script

--- Synthetic_Payment_Technology_Data.py
+++ Synthetic_Payment_Technology_Data.py
@@ -8,8 +8,8 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from xgboost import XGBRegressor
 
-# Load data from your file
-file_path = '/Users/pconnor/Downloads/Synthetic_Payment_Technology_Data.csv'
+# Load data from the local file
+file_path = 'Synthetic_Payment_Technology_Data.csv'
 df = pd.read_csv(file_path)
 
 # Display the first few rows of the dataframe
